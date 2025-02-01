59444357154f1e1a92a658f5e322878bbf99ae37
yunhwan-dev <wjsdbsghks96@dgu.ac.kr>
2025-02-02 07:57:41 +0900

    feat: 로그 추적기 v2 파라미터로 동기화 개발

---
+++ advanced/src/main/kotlin/com/example/advanced/trace/hellotrace/HelloTraceV2.kt
@@ -0,0 +1,61 @@
+package com.example.advanced.trace.hellotrace
+
+import com.example.advanced.trace.TraceId
+import com.example.advanced.trace.TraceStatus
+import org.springframework.stereotype.Component
+
+
+@Component
+class HelloTraceV2 {
+
+    fun begin(message: String): TraceStatus {
+        val traceId = TraceId()
+        val startTimeMs = System.currentTimeMillis()
+        println("[${traceId.id}] ${addSpace(START_PREFIX, traceId.level)}$message")
+        return TraceStatus(traceId, startTimeMs, message)
+    }
+
+    //v2 추가
+    fun beginSync(beforeTraceV1: TraceId, message: String): TraceStatus {
+        val traceId = beforeTraceV1.createNextId()
+        val startTimeMs = System.currentTimeMillis()
+        println("[${traceId.id}] ${addSpace(START_PREFIX, traceId.level)}$message")
+        return TraceStatus(traceId, startTimeMs, message)
+    }
+
+
+    fun end(status: TraceStatus) {
+        complete(status, null)
+    }
+
+    fun exception(status: TraceStatus, e: Exception?) {
+        complete(status, e)
+    }
+
+    fun complete(status: TraceStatus, e: Exception?) {
+        val stopTimeMs = System.currentTimeMillis()
+        val resultTimeMs = stopTimeMs - status.startTimeMs
+        val traceId = status.traceId
+
+        if (e == null) {
+            println("[${traceId.id}] ${addSpace(END_PREFIX, traceId.level)}${status.message} time=${resultTimeMs}ms")
+        } else {
+            println("[${traceId.id}] ${addSpace(EXCEPTION_PREFIX, traceId.level)}${status.message} time=${resultTimeMs}ms ex=${e}")
+        }
+    }
+
+
+    fun addSpace(prefix: String, level: Int): String {
+        return buildString {
+            repeat(level) { i ->
+                append(if (i == level - 1) "|$prefix" else "|   ")
+            }
+        }
+    }
+
+    companion object {
+        const val START_PREFIX = "-->"
+        const val END_PREFIX = "<--"
+        const val EXCEPTION_PREFIX = "<X--"
+    }
+}
