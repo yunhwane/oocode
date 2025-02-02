73f1420babad2e5cd7775278ba083f50ebd0db5c
yunhwan-dev <wjsdbsghks96@dgu.ac.kr>
2025-02-02 22:01:55 +0900

    feat: 구체 클래스 기반 예제 추가

---
+++ proxy/src/test/java/com/example/proxy/pureproxy/concreteproxy/code/ConcreteLogic.java
@@ -0,0 +1,13 @@
+package com.example.proxy.pureproxy.concreteproxy.code;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ConcreteLogic {
+
+    public String operation() {
+        log.info("concrete logic operation");
+        return "data";
+    }
+}
