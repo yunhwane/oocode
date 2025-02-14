0ee823f84c6536caaf89a3c69140a7def44a26ea
yunhwan <147581818+yunhwane@users.noreply.github.com>
2025-02-14 17:26:09 +0900

    feat: 제네릭과 타입 파라미터

---
+++ kotlin-advenced/src/generic/Cage.kt
@@ -0,0 +1,35 @@
+package generic
+
+
+
+class Cage {
+    private val animals: MutableList<Animal> = mutableListOf()
+
+    fun getFirst(): Animal {
+        return animals.first()
+    }
+
+    fun put(animal: Animal) {
+        animals.add(animal)
+    }
+
+    fun moveFrom(cage: Cage) {
+        this.animals.addAll(cage.animals)
+    }
+}
+
+class Cage2<T> {
+    private val animals: MutableList<T> = mutableListOf()
+
+    fun getFirst(): T {
+        return animals.first()
+    }
+
+    fun put(animal: T) {
+        animals.add(animal)
+    }
+
+    fun moveFrom(cage: Cage2<T>) {
+        this.animals.addAll(cage.animals)
+    }
+}
